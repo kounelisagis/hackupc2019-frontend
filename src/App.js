f5dabcd26d1289fedc847f88425fb77b271573f8
Agisilaos Kounelis <36283973+kounelisagis@users.noreply.github.com>
2019-10-12 13:06:51 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+  state = {
+    textValue: 'Upload your photo!'
+  }
+
+  onChangeHandler=event => {
+    this.setState({
+      textValue: 'Uploading'
+    })
+
+    // create a new XMLHttpRequest
+    var xhr = new XMLHttpRequest()
+
+    // get a callback when the server responds
+    xhr.addEventListener('load', () => {
+      // update the state of the component with the result here
+      console.log(xhr.responseText)
+    })
+    // open the request with the verb and the url
+    xhr.open('POST', 'localhosttttttt')
+    xhr.send(JSON.stringify({ image: event.target.files[0]}))
+    // send the request
+    xhr.send()
+  }
+
+
+  render() {
+    return (
+      <div id="container">
+        <p id="load_text">{this.state.textValue}</p>
+        <input id="myFileInput" type="file" accept="image/*" capture="camera" onChange={this.onChangeHandler}></input>
+      </div>
+    )
+  }
 }
-
-export default App;
